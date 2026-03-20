3b83844f7d52f72e5c5bbb944def3258bf647fa9
RAMJAN <niloyahmadramjan@gmail.com>
2026-03-21 01:37:20 +0800

    created proxy.js file and handle the auth protext route

---
+++ proxy.ts
@@ -0,0 +1,38 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  const publicRoute = [
+    "/login",
+    "/register",
+    "/api/auth",
+    "/favicon.ico",
+    "/_next",
+  ];
+  if (publicRoute.some((path) => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+  if (!token) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("callbackeUrl", req.url);
+    return NextResponse.redirect(loginUrl);
+  }
+  return NextResponse.next();
+}
+// will be not run this middleware if below action
+export const config = {
+  matcher:
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|images).*)",
+};
+
+/**
+ *  req---middleware---server
+ *
+ * login register api auth public route direact sent to server
+ *
+ */
